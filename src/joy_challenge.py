4a88ffb2836041071c4eebd852b294733ccbc8a4
Your Name <you@example.com>
2023-08-31 16:49:13 -0400

    Terminar joy_challenge

--- src/joy_challenge.py
+++ src/joy_challenge.py
@@ -4,41 +4,37 @@ import rospy #importar ros para python
 from std_msgs.msg import String, Int32 #importa mensajes de ROS tipo String y Int32
 from sensor_msgs.msg import Joy # impor mensaje tipo Joy
 from geometry_msgs.msg import Twist # importar mensajes de ROS tipo geometry / Twist
-from duckietown_msgs.msg import Twist2DStamped 
+from duckietown_msgs.msg import Twist2DStamped
 
 
 class Template(object):
     def __init__(self, args):
         super(Template, self).__init__()
         self.args = args
-        #sucribir a joy 
-        self.sub = rospy.Subscriber("topic" , Joy, self.callback)
+        #sucribir a joy
+        self.sub = rospy.Subscriber("/duckiebot/joy" , Joy, self.callback)
         #publicar la intrucciones del control en possible_cmd
-        self.publi = rospy.Publisher("topic", Twist2DStamped, queue_size = "x")
+        self.publi = rospy.Publisher("/duckiebot/wheels_driver_node/car_cmd", Twist2DStamped, queue_size = 10)
         self.twist = Twist2DStamped()
 
 
-    #def publicar(self, msg):
-        #self.publi.publish(msg)
+    def publicar(self, msg):
+        self.publi.publish(msg)
 
     def callback(self,msg):
-        a = msg.buttons[]
-        y = msg.axes[]
-        x = msg.axes[]
-        z = msg.axes[]
+
+        #a = msg.buttons[]
+        #y = msg.axes[1]
+        x = msg.axes[0]
+        z = (1 - msg.axes[5])/2 #boton RT
+        z2 = (1 - msg.axes[2])/2 #boton LT
 
         print(y, x, z)
-        self.twist.omega = 
-        self.twist.v = 
+        self.twist.omega = -x * 10
+        self.twist.v = z - z2
 
-        if a == 1:
-            self.twist.omega = 
-            self.twist.v = 
-        
+
         self.publi.publish(self.twist)
-        
-
-
 
 
 def main():
@@ -46,7 +42,7 @@ def main():
 
     obj = Template('args') # Crea un objeto del tipo Template, cuya definicion se encuentra arriba
 
-    #objeto.publicar() #llama al metodo publicar del objeto obj de tipo Template
+    #obj.publicar() #llama al metodo publicar del objeto obj de tipo Template
 
     rospy.spin() #funcion de ROS que evita que el programa termine -  se debe usar en  Subscribers
 
